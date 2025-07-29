e20f193efd33a75b33675eeb0fc00b0489259d00
Muri <thomas.muranaka@wassermeloni.de>
2025-07-29 20:26:14 +0200

    logging hinzugefügt

--- models/partner.py
+++ models/partner.py
@@ -1,3 +1,7 @@
+import logging
+_logger = logging.getLogger(__name__)
+from odoo import models
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
@@ -5,19 +9,24 @@ class ResPartner(models.Model):
         KONTEN_START = 101100000
         KONTEN_MAX = 109999999
 
+        Account = self.env['account.account']
+        account_type = self.env.ref('account.data_account_type_receivable')
+
         for partner in self:
             if not partner.property_account_receivable_id:
-                Account = self.env['account.account']
                 existing_accounts = Account.search([
                     ('code', '>=', str(KONTEN_START)),
                     ('code', '<=', str(KONTEN_MAX)),
                 ], order='code desc', limit=1)
 
                 next_number = int(existing_accounts.code) + 1 if existing_accounts else KONTEN_START
+                # Sicherstellen, dass der nächste Code noch frei ist
                 while Account.search_count([('code', '=', str(next_number))]) > 0 and next_number <= KONTEN_MAX:
                     next_number += 1
+
                 if next_number <= KONTEN_MAX:
                     konto_nummer = str(next_number)
+                    # Name: Nachname, Vorname (Fallback: Partner-Name)
                     if hasattr(partner, "lastname") and hasattr(partner, "firstname") and partner.lastname and partner.firstname:
                         konto_name = f"{partner.lastname}, {partner.firstname}"
                     else:
@@ -25,8 +34,15 @@ class ResPartner(models.Model):
                     account = Account.create({
                         'code': konto_nummer,
                         'name': konto_name,
-                        'user_type_id': self.env.ref('account.data_account_type_receivable').id,
+                        'user_type_id': account_type.id,
                         'reconcile': True,
                         'company_id': partner.company_id.id if partner.company_id else self.env.company.id,
                     })
+                    _logger.info("Debitorenkonto %s angelegt", konto_nummer)
                     partner.property_account_receivable_id = account.id
+                    _logger.info("Debitorenkonto %s dem Partner %s zugewiesen.", konto_nummer, konto_name)
+                else:
+                    # Optional: Fehler werfen oder warnen, falls alle Nummern belegt
+                    _logger.warning('Alle Debitorennummern vergeben!')
+                    pass
+        return True
